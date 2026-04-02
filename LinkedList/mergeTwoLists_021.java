cb9de8dc1e0bf91dd55bbbb970712ce18f3f139b
ThinhTran <15520847@gm.uit.edu.vn>
2026-04-02 17:50:25 +0700

    Add LeetCode 021: Merge Two Sorted Lists

---
+++ LinkedList/mergeTwoLists_021.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class mergeTwoLists_021 {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else {
+            curr.next = list2;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Create list1: 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        // Create list2: 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode result = mergeTwoLists(l1, l2);
+        System.out.print("Kết quả trộn hai danh sách: ");
+        printList(result);
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
