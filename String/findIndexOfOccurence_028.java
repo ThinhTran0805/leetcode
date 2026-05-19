a681485b905783212c5ca3587d7918ef0a59a789
ThinhTran <15520847@gm.uit.edu.vn>
2026-05-19 22:39:58 +0700

    2026-05-19: Solve LeetCode 28 - Find the Index of the First Occurrence in a String

---
+++ String/findIndexOfOccurence_028.java
@@ -0,0 +1,30 @@
+public class findIndexOfOccurence_028 {
+    public static void main(String[] args) {
+        findIndexOfOccurence_028 sol = new findIndexOfOccurence_028();
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        int result = sol.findIndex(haystack, needle);
+        System.out.println(result);
+    }
+
+    public int findIndex(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        int limit = haystack.length() - needle.length();
+
+        for (int i = 0; i <= limit; i++) {
+            int j;
+            for (j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
